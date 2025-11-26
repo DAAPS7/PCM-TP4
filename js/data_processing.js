548119c4efdfd6fc07929e06017f063f12cb14a6
David Santos <39568173+DAAPS7@users.noreply.github.com>
2025-11-26 00:25:06 +0000

    data process almost done

--- js/data_processing.js
+++ js/data_processing.js
@@ -6,14 +6,22 @@ $(document).ready(function () {
 
 function cleanElectionData(rawData) {
   const cleaned = {};
-  const distritosArray = Object.entries(rawData[0]);
 
-  distritosArray.forEach(([codigo, nome]) => {
-    rawData.forEach((row) => {
-      const item = row["Círculo"];
-      console.log(item);
-    });
+  const districtsArray = Object.entries(rawData[0]); // Districts and Codes ([1, 'Aveiro'])
+
+  rawData.forEach((row) => {
+    if (row && row["Círculo"]) {
+      const item = row["Círculo"]; // Item (Inscritos, Votantes, etc.)
+      cleaned[item] = {}; // Initializes the item in the cleaned JSON
+
+      // TODO: Remove the items that don't have relevant data
+
+      districtsArray.forEach(([code, name]) => {
+        cleaned[item][name] = row[code]; // Coloca na posição do item atual o nome do distrito e o seu número baseado no seu código
+      });
+    }
   });
 
+  console.log(cleaned);
   return cleaned;
 }
