86973dc9654c5cc9088d7498c646cceb72ec3333
David Santos <daapsyt@gmail.com>
2025-11-29 22:26:52 +0000

    json fix

--- election_dashboard/js/data_processing.js
+++ election_dashboard/js/data_processing.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $.getJSON("../data/legislativas_raw.json", function (data) {
+  $.getJSON("data/legislativas_raw.json", function (data) {
     //const obj = cleanElectionData(data);
     //exportJSON(obj);
   });
@@ -15,26 +15,33 @@ function cleanElectionData(rawData) {
       const item = row["Círculo"]; // Item (Inscritos, Votantes, etc.)
 
       if (isItemValid(item)) {
+        let empty = true;
         cleaned[item] = {}; // Initializes the item in the cleaned JSON
 
         districtsArray.forEach(([code, name]) => {
           cleaned[item][name] = row[code]; // Coloca na posição do item atual o nome do distrito e o seu número baseado no seu código
+          if (row[code]) empty = false;
         });
+
+        if (empty) {
+          delete cleaned[item];
+        }
       }
     }
   });
 
-  console.log(cleaned);
   return cleaned;
 }
 
 // Validate item
 function isItemValid(item) {
   return (
-    !item.includes("-") &&
+    !item.includes("md") &&
     !item.includes("*") &&
     !item.includes("Observações") &&
-    !item.includes("somatórios")
+    !item.includes("somatórios") &&
+    !item.includes("%") &&
+    !item.includes("VVE")
   );
 }
 
